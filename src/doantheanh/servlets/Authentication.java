fae37df1953cf1789408465b93d500aea6cbd0b5
vTigerCRM_91 <contact.dfr.hta@gmail.com>
2017-07-07 10:45:47 +0700

    cookie

--- src/doantheanh/servlets/Authentication.java
+++ src/doantheanh/servlets/Authentication.java
@@ -2,10 +2,12 @@ package doantheanh.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -42,6 +44,8 @@ public class Authentication extends HttpServlet {
 		DatabaseManagement dm = new DatabaseManagement();
 		
 		if (dm.checkUser(userName, password)) {
+			response.addCookie(new Cookie("location", "VietNam"));
+			
 			HttpSession session = request.getSession();
 			//session.setMaxInactiveInterval(900);setTimeOut by minisecon
 			
--- src/doantheanh/servlets/ImageShow.java
+++ src/doantheanh/servlets/ImageShow.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -53,6 +54,11 @@ public class ImageShow extends HttpServlet {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
 			dispatcher.forward(request, response);
 		} else {
+			for(Cookie c: request.getCookies()) {
+				System.out.println(c.getName());
+				System.out.println(c.getValue());
+			}
+			
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/baby2.jsp");
 			dispatcher.forward(request, response);
 		}
